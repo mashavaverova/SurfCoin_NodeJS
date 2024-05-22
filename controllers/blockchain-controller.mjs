c21fabbc60fbe5a412d3053792bdd7cf75a47995
mashavaverova <129207381+mashavaverova@users.noreply.github.com>
2024-05-23 01:11:17 +0200

    redis-server setup, block- and blockchain-controllers edtinig

--- controllers/blockchain-controller.mjs
+++ controllers/blockchain-controller.mjs
@@ -1,3 +1,9 @@
+import { blockchain } from '../server.mjs';
 
-
-export const listBlocks = async (req, res, next) => {}
+export const listBlocks = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    statusCode: 200,
+    data:  blockchain.chain
+  });
+};
--- redis-server.mjs
+++ redis-server.mjs
@@ -1,8 +1,52 @@
 import redis from 'redis';
 
 const CHANNELS = {
-    TEST: 'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN',
+  TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
 };
 
-export default class RedisServer {}
+// redis server for broadcasting blockchain to clients 
+export default class RedisServer {
+  constructor() {
+    this.blockchain = blockchain;
+    this.publisher = redis.createClient();
+    this.subscriber = redis.createClient();
+
+    this.loadChannels();
+
+    this.subscriber.on('message', (channel, message) =>
+      this.messageHandler(channel, message)
+    );
+  }
+
+  //broadcast blockchain 
+  broadcast() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
+  }
+
+  //helper functions
+  //1. load channels to subscribe to messages from the redis server
+  loadChannels() {
+    Object.values(CHANNELS).forEach((channel) =>
+      this.subscriber.subscribe(channel)
+    );
+  }
+
+  //2. message handler for the redis server
+  messageHandler(channel, message) {
+    const message = JSON.parse(message);
+
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      console.log('REPLACE IS IN PROGRESS', message);
+      this.blockchain.replaceChain(message);
+    }
+  }
+
+  //3. send message to redis server
+  publish({ channel, message }) {
+    this.publisher.publish(channel, message);
+  }
+}
