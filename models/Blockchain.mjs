cf5370a25c19c58711ab7869e00ba097c09cb98d
mashavaverova <129207381+mashavaverova@users.noreply.github.com>
2024-05-23 22:11:04 +0200

    blockchain_test and Blockchain.mjs

---
+++ models/Blockchain.mjs
@@ -0,0 +1,49 @@
+import Block from '../models/Block.mjs';
+import { createHash } from '../utilities/crypto-lib.mjs';
+export default class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis];
+  }
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain.at(-1),
+      data: data,
+    });
+
+    this.chain.push(newBlock);
+    return newBlock;
+  }
+
+
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) return;
+    if (!Blockchain.validateChain(chain)) return;
+    this.chain = chain;
+  }
+
+
+  static validateChain(chain) {
+    if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis)) {
+      return false;
+    }
+
+    for (let i = 1; i < chain.length; i++) {
+      const { timestamp, lastHash, hash, data, difficulty, nonce } = chain[i];
+      const actualLastHash = chain[i - 1].hash;
+      const lastDifficulty = chain[i - 1].difficulty;
+
+      if (lastHash !== actualLastHash) return false;
+      if (Math.abs(lastDifficulty - difficulty) > 1) return false;
+      const validHash = createHash(
+        timestamp,
+        lastHash,
+        data,
+        difficulty,
+        nonce
+      );
+      if (hash !== validHash) return false;
+    }
+
+    return true;
+  }
+}
