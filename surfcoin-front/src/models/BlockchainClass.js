0d87b824f5ccd6546c5f500ea3f5805e2dd0780c
natlee77 <70273134+natlee77@users.noreply.github.com>
2024-06-01 16:35:21 +0200

    rensad

--- surfcoin-front/src/models/BlockchainClass.js
+++ surfcoin-front/src/models/BlockchainClass.js
@@ -2,22 +2,9 @@
 // import Block from  './BlockClass.js';
 
  export default class Blockchain {
-//   constructor() {
-//     this.chain = [Block.genesis];
-//   }
-
-//   // Instance method...
-//   addBlock({ data }) {
-//     const newBlock = Block.mineBlock({
-//       lastBlock: this.chain.at(-1),
-//       data: data,
-//     });
-//     this.chain.push(newBlock);
-//     return newBlock;
-//   }
-
-   
-
-   
+    constructor() {
+      this.chain = [Block.genesis];
+   }
+  
     
   }
